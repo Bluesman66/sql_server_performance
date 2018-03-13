33618b33e5d16a11f33dfaef4b4c2325d48baef8
alex.paclin <alex.paclin@gmail.com>
2018-03-13 18:28:20 +0500

    SQLQuery2

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+select co.*
+	from CourseOfferings co
+	left outer join CourseEnrollments ce
+		on co.CourseOfferingId = ce.CourseOfferingId
+	where co.TermCode = 'SP2016' and
+		ce.CourseOfferingId is null;
