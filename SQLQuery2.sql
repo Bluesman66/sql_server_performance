a4975b580cb3babba82ebe94139fc46f0b5faae8
alex.paclin <alex.paclin@gmail.com>
2018-03-14 11:03:18 +0500

    SQLQuery2 joins can be rewritten as subqueries

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,4 +3,15 @@ select co.*
 	left outer join CourseEnrollments ce
 		on co.CourseOfferingId = ce.CourseOfferingId
 	where co.TermCode = 'SP2016' and
-		ce.Grade is null;
+		ce.CourseOfferingId is null;
+
+
+select co.*
+	from CourseOfferings co
+	where not exists
+	(
+		select 1
+			from CourseEnrollments ce
+			where ce.CourseOfferingId = co.CourseOfferingId 
+	)
+	and co.TermCode = 'SP2016';
